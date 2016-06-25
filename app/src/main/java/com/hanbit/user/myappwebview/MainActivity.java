5fba716e02bf5670d3942f1f63eb8602f71ed9da
nohseunghee <rshnar@hanmail.net>
2016-06-25 12:27:10 +0900

    2016-06-25

---
+++ app/src/main/java/com/hanbit/user/myappwebview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hanbit.user.myappwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.naver.com");
+    }
+}
